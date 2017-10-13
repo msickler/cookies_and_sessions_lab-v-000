1dfa6c2ff8e9be4afb1881f1126218f91860e188
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 23:24:04 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,8 @@ class ProductsController < ApplicationController
   end
 
   def add
-    cart << params[:product]
+    @product = (params[:id])
+    cart << @product
     redirect_to root_path
   end
 
